b3a926e98bc9cb88cfc5163075becfd9d8e3e6b2
Jesús Manuel Bañuelos Ibarra <63622195+jejejesus@users.noreply.github.com>
2023-10-11 17:25:25 -0700

    Iterated tried

--- queens/main.py
+++ queens/main.py
@@ -1,5 +1,15 @@
 from board import board, dls
 
+def iterated_dls():
+    limit = 2
+
+    for i in range(20):
+        game = board(size=4, queens=[0, 0, 0, 0])
+        frontier = []
+        frontier.append(game)
+        print(dls(frontier, limit=limit))
+        game.visited = []
+
 def main():
     game = board(size=4, queens=[0, 0, 0, 0])
     frontier = []
