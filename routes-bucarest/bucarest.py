858b4fdab64a192c278c1fc5542429e7144e3c93
Jesús Manuel Bañuelos Ibarra <63622195+jejejesus@users.noreply.github.com>
2023-10-18 22:08:58 -0700

    Inicio Bucarest

---
+++ routes-bucarest/bucarest.py
@@ -0,0 +1,47 @@
+class to_bucarest:
+    routes = [[None,None,None,None,None,None,None,None,None,None,None,None,None,None,None, 140, 118,None,None,  75],
+              [None,None,None,None,None, 211,  90,None,None,None,None,None,None, 101,None,None,None,  85,None,None],
+              [None,None,None, 120,None,None,None,None,None,None,None,None,None, 138, 146,None,None,None,None,None],
+              [None,None, 120,None,None,None,None,None,None,None,  75,None,None,None,None,None,None,None,None,None],
+              [None,None,None,None,None,None,None,  86,None,None,None,None,None,None,None,None,None,None,None,None],
+              [None, 211,None,None,None,None,None,None,None,None,None,None,None,None,None,  99,None,None,None,None],
+              [None,  90,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
+              [None,None,None,None,  86,None,None,None,None,None,None,None,None,None,None,None,None,  98,None,None],
+              [None,None,None,None,None,None,None,None,None,None,None,  87,None,None,None,None,None,None,  92,None],
+              [None,None,None,None,None,None,None,None,None,None,  70,None,None,None,None,None, 111,None,None,None],
+              [None,None,None,  75,None,None,None,None,None,  70,None,None,None,None,None,None,None,None,None,None],
+              [None,None,None,None,None,None,None,None,  87,None,None,None,None,None,None,None,None,None,None,None],
+              [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None, 151,None,None,None,  71],
+              [None, 101, 138,None,None,None,None,None,None,None,None,None,None,None,  97,None,None,None,None,None],
+              [None,None, 146,None,None,None,None,None,None,None,None,None,None,  97,None,  80,None,None,None,None],
+              [ 140,None,None,None,None,  99,None,None,None,None,None,None, 151,None,  80,None,None,None,None,None],
+              [ 118,None,None,None,None,None,None,None,None, 111,None,None,None,None,None,None,None,None,None,None],
+              [None,  85,None,None,None,None,None,  98,None,None,None,None,None,None,None,None,None,None, 142,None],
+              [None,None,None,None,None,None,None,None,  92,None,None,None,None,None,None,None,None, 142,None,None],
+              [  75,None,None,None,None,None,None,None,None,None,None,None,  71,None,None,None,None,None,None,None]]
+    
+    lineal_distance = {
+        "Arad": 266,
+        "Bucarest": 0,
+        "Craiova": 160,
+        "Dovreta": 242,
+        "Eforie": 161,
+        "Fagaras": 176,
+        "Giurgiu": 77,
+        "Hirsova": 151,
+        "Iasi": 226,
+        "Lugoj": 244,
+        "Mehadia": 241,
+        "Neamt": 234,
+        "Oradea": 380,
+        "Pitesti": 100,
+        "Rimnicu": 193,
+        "Sibiu": 253,
+        "Timisoara": 329,
+        "Urziceni": 80,
+        "Vaslui": 199,
+        "Zerind": 374
+    }
+
+    def __init__(self) -> None:
+        pass
---
+++ routes-bucarest/main.py
@@ -0,0 +1,7 @@
+from bucarest import to_bucarest
+
+def main():
+    buca = to_bucarest()
+
+if __name__ == "__main__":
+    main()
