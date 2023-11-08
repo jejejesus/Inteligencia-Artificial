136f932ca90a8c61df8515c0db9541524b34c775
Jesús Manuel Bañuelos Ibarra <63622195+jejejesus@users.noreply.github.com>
2023-11-07 22:33:44 -0700

    Llenat tabla a estresha

---
+++ romania - copia/main.py
@@ -0,0 +1,12 @@
+from romania import city, a_star_search, route_str
+
+def main(): # Método main
+    origin = "Andorra"
+    initial_city = city(origin) # Se declara la ciudad inicial
+    frontier:list[city] = []
+    frontier.append(initial_city) # Se agrega a la frontera
+    message, goal = a_star_search(frontier) # Llamanos a la función de búsqueda y recibimos un mensaje y la ciudad destino con la ruta recorrida
+    print("\n" + message + ": " + route_str(goal) + "\n")
+
+if __name__ == "__main__":
+    main()
--- romania - copia/romania.py
+++ romania - copia/romania.py
@@ -5,10 +5,10 @@ routes = {
     'Berlin': {'Amsterdam': 657,'Bern': 956, 'Brussels': 868, 'Luxembourg': 763, 'Rome': 1503, 'Vienna': 681, 'Warsaw': 574},
     'Bern': {'Andorra': 1044, 'Berlin': 957, 'Luxembourg': 447, 'Paris': 564, 'Rome': 851, 'Vienna': 944, 'Warsaw': 1444},
     'Brussels': {'Amsterdam': 210, 'Berlin': 868, 'Luxembourg': 224, 'Paris': 312},
-    'Lisbon': {'Madrid': 633},
+    'Lisbon': {'Madrid': 633, 'Paris': 1736},
     'Luxembourg': {'Amsterdam': 391, 'Berlin': 763, 'Bern': 447, 'Brussels': 763, 'Paris': 373, 'Vienna': 939},
     'Madrid': {'Andorra': 619, 'Lisbon': 633, 'Paris': 1275},
-    'Paris': {'Andorra': 864, 'Bern': 564, 'Brussels': 312, 'Luxembourg': 373, 'Madrid': 1275},
+    'Paris': {'Andorra': 864, 'Bern': 564, 'Brussels': 312, 'Lisbon': 1736, 'Luxembourg': 373, 'Madrid': 1275},
     'Rome': {'Andorra': 1385, 'Bern': 851, 'Vienna': 1122},
     'Vienna': {'Amsterdam': 1145, 'Berlin': 681, 'Bern': 944, 'Luxembourg': 939, 'Rome': 1122, 'Warsaw': 727},
     'Warsaw': {'Berlin': 574, 'Bern': 1444, 'Vienna': 727}              
@@ -29,74 +29,45 @@ lineal_distance = {
     'Warsaw': {'Amsterdam': 1096, 'Andorra': 1816, 'Berlin': 517, 'Bern': 1138, 'Brussels': 1160, 'Lisbon': 2759, 'Luxembourg': 1080, 'Madrid': 2289, 'Paris': 1366, 'Rome': 1310, 'Vienna': 553, 'Warsaw': 0}
 }
 
-# Tabla A*
-a_star = {
-    'Amsterdam': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Andorra': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Berlin': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Bern': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Brussels': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Lisbon': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Luxembourg': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Madrid': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Paris': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Rome': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Vienna': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0},
-
-    'Warsaw': {'Amsterdam': 0, 'Andorra': 0, 'Berlin': 0, 'Bern': 0, 'Brussels': 0, 'Lisbon': 0, 'Luxembourg': 0, 'Madrid': 0, 'Paris': 0, 'Rome': 0, 'Vienna': 0, 'Warsaw': 0}
-}
-
-origin_city = "Paris"
+destine_city = "Amsterdam"
 
 class city: # Clase City
+    city_name:str = ""
+    distance_traveled:int = 0
 
-    def __init__(self, city_name:str, previous_city = None, cities_traveled:int = 1, distance_traveled:int = 0) -> None:
+    def __init__(self, city_name:str, previous_city = None, distance_traveled:int = 0) -> None:
         self.city_name = city_name
         self.previous_city = previous_city
-        self.cities_traveled = cities_traveled
         self.distance_traveled = distance_traveled
 
-    def goal_test(self) -> bool: # Función Goal Test para saber si estamos en la ciudad en la que empezamos y ya recorrimos 8 ciudades
-        return self.city_name == origin_city and self.cities_traveled == 9
-    
+    def goal_test(self) -> bool: # Función Goal Test
+        return self.city_name == destine_city
     def expand(self) -> list: # Función Expand, en esta también se calcula la distancia total viaja de cada off spring
         off_springs:list[city] = []
-        #visited = visited_list(self)
         for off_spring in routes[self.city_name]: # Por cada ruta que tenga la ciudad...
-            #if off_spring not in visited:
             new_distance:int = self.distance_traveled + routes[self.city_name][off_spring] # Calculamos la nueva distancia recorrida a cada offspring
-            off_springs.append(city(city_name=off_spring, previous_city=self, cities_traveled=self.cities_traveled + 1, distance_traveled=new_distance)) # Agregamos al offspring a la lista de offsprings
+            off_springs.append(city(city_name=off_spring, previous_city=self, distance_traveled=new_distance)) # Agregamos al offspring a la lista de offsprings
         return off_springs
     
     def heuristic(self) -> int: # Función para calcular la herística
-        return self.distance_traveled + lineal_distance[origin_city][self.city_name]
+        return self.distance_traveled + lineal_distance[destine_city][self.city_name]
 
     def __repr__(self) -> str:
-        return f"City: {self.city_name:10}, Traveled distance: {str(self.distance_traveled):>5}, Heuristic: {str(self.heuristic()):>5}, Cities traveled: {str(self.cities_traveled):>3}{f', Previous city: {self.previous_city.city_name}' if self.previous_city != None else ''}"
+        return f"City: {self.city_name:10}, Traveled distance: {str(self.distance_traveled):>5}, Heuristic: {str(self.heuristic()):>5}{f', Previous city: {self.previous_city.city_name}' if self.previous_city != None else ''}"
 
 def route_str(end_city:city, route:str = "") -> str: # Función que recibe una ciudad y retorna un string con la ruta que se tomó para llegar a ella
-    route = (end_city.city_name + "(" + str(end_city.distance_traveled) + ") -> ") + route
+    route = (end_city.city_name + "city(\"" + str(end_city.distance_traveled) + "\", previos_city=") + route + ")"
     if end_city.previous_city != None:
         return route_str(end_city.previous_city, route)
     else:
-        return route[0:-4]
+        return route[0:-4] + ""
 
-def visited_list(end_city:city, route:list[str] = []) -> list[str]: # Función que recibe una ciudad y retorna un lista con la ruta que se tomó para llegar a ella        
-        if end_city.city_name not in route: route.append(end_city.city_name)
-        if end_city.previous_city != None:
-            return visited_list(end_city.previous_city, route)
-        else:
-            return route[1:]
+def route_list(end_city:city, route:list[str] = []) -> list[str]: # Función que recibe una ciudad y retorna un lista con la ruta que se tomó para llegar a ella
+    route.append(end_city.city_name)
+    if end_city.previous_city != None:
+        return route_list(end_city.previous_city, route)
+    else:
+        return route
 
 def a_star_search(frontier:list[city]) -> tuple[str, city]: # Función de búsqueda A*
     if frontier == []: # Verificamos si frontier está vacío
@@ -105,10 +76,8 @@ def a_star_search(frontier:list[city]) -> tuple[str, city]: # Función de búsqu
     print(current_city)
     if current_city.goal_test(): # Verificamos si la ciudad actual es la meta
         return "Solution found", current_city
-    if current_city.cities_traveled > 8:
-        return "Limit reached", None
     off_springs = current_city.expand() # Obtenemos los offsprings de la ciudad actual
     frontier += off_springs # Agregamos los offsprings a frontier
     frontier.sort(key=city.heuristic) # Ordenamos frontier según la heurística
 
-    return a_star_search(frontier)
+    return a_star_search(frontier)        
